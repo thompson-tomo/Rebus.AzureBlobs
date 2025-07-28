af104f61282670422cb6349cc3ccff5ee22d5aec
Mogens Heller Grabe <mookid8000@gmail.com>
2025-07-28 10:02:31 +0200

    about to reproduce this

---
+++ Rebus.AzureBlobs.Tests/Bugs/ReproduceLostMetadataIssue.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+using NUnit.Framework;
+using Rebus.AzureBlobs.DataBus;
+using Rebus.AzureBlobs.Tests.DataBus;
+using Rebus.Config;
+using Rebus.Logging;
+using Rebus.Tests.Contracts;
+
+namespace Rebus.AzureBlobs.Tests.Bugs;
+
+[TestFixture]
+public class ReproduceLostMetadataIssue : FixtureBase
+{
+    [Test]
+    public async Task CustomMetadataLost()
+    {
+        var loggerFactory = new ConsoleLoggerFactory(colored: false);
+        var fakeRebusTime = new FakeRebusTime();
+        var options = new AzureBlobsDataBusStorageOptions();
+        var client = new BlobContainerClient(AzureConfig.ConnectionString, "test");
+
+        var storage = new AzureBlobsDataBusStorage(loggerFactory, fakeRebusTime, options, client);
+
+
+    }
+}
